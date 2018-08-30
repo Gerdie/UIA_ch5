016c7bd3364ed969aaeb1e2a6c7907f2820ca841
Gerdie <mariakmoy@gmail.com>
2018-08-29 21:07:48 -0700

    SceneController and MemoryCard scripts

---
+++ Assets/MemoryCard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour {
+	[SerializeField] private GameObject cardBack;
+	[SerializeField] private Sprite image;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<SpriteRenderer> ().sprite = image;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnMouseDown() {
+		if (cardBack.activeSelf) {
+			cardBack.SetActive (false);
+		}
+	}
+
+}
