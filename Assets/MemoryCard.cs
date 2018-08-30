0cb78d055d7de981a87bf83c9c49239c2a088a28
Gerdie <mariakmoy@gmail.com>
2018-08-29 22:05:49 -0700

    set the starting positions

--- Assets/MemoryCard.cs
+++ Assets/MemoryCard.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class MemoryCard : MonoBehaviour {
 	[SerializeField] private GameObject cardBack;
 	[SerializeField] private Sprite image;
+	private int _id;
+
+	public int id {
+		get {return _id;}
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -14,6 +19,11 @@ public class MemoryCard : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void SetCard(int id, Sprite image) {
+		_id = id;
+		GetComponent<SpriteRenderer> ().sprite = image;
 	}
 
 	public void OnMouseDown() {
--- Assets/SceneController.cs
+++ Assets/SceneController.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SceneController : MonoBehaviour {
+	[SerializeField] private Sprite[] images;
+	private MemoryCard[] deck;
+	public MemoryCard card;
 
 	// Use this for initialization
 	void Start () {
-		
+		float[] xPositions = {-3f, 0f, 3f};
+		float[] yPositions = {-1.8f, 1.8f};
+		deck = new MemoryCard[xPositions.Length * yPositions.Length];
+		int cardIndex = 0;
+
+		for (int xIndex=0; xIndex < xPositions.Length; xIndex++) {
+			for (int yIndex = 0; yIndex < yPositions.Length; yIndex++) {
+				MemoryCard cardClone;
+				cardClone = Instantiate(card) as MemoryCard;
+				// choose a random card image
+				cardClone.SetCard(cardIndex, images[Random.Range(0, images.Length)]);
+				// set position
+				cardClone.transform.position = new Vector3 (xPositions [xIndex], yPositions [yIndex], 0);
+				// add to deck array
+				deck [cardIndex] = cardClone;
+			}
+		}
+
 	}
 	
 	// Update is called once per frame
